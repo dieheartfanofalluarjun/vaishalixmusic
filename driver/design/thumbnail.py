7d826a382a5f23eac8b50e6605bc8bed4dc715a5
Santhosh podili <99010597+Santhupodili@users.noreply.github.com>
2022-03-17 19:48:42 +0530

    Update thumbnail.py

--- driver/design/thumbnail.py
+++ driver/design/thumbnail.py
@@ -1,6 +1,7 @@
 import os
 import aiofiles
 import aiohttp
+from config import BOT_NAME
 from PIL import (
     Image,
     ImageDraw,
@@ -32,11 +33,12 @@ async def thumb(thumbnail, title, userid, ctitle):
     image6 = image4.convert("RGBA")
     Image.alpha_composite(image5, image6).save(f"search/temp{userid}.png")
     img = Image.open(f"search/temp{userid}.png")
-    draw = ImageDraw.Draw(img)
+    draw = ImageDraw.Draw(img) 
     font = ImageFont.truetype("driver/source/finalfont.ttf", 85)
     font2 = ImageFont.truetype("driver/source/finalfont.ttf", 60)
+    arial = ImageFont.truetype("driver/source/finalfont.ttf", 60)
      draw.text(
-            (5, 5), f"{MUSIC_BOT_NAME}", fill="white", font=name_font
+            (5, 5), f"{BOT_NAME}", fill="white", font=name_font
         )
         draw.text(
             (600, 150),
