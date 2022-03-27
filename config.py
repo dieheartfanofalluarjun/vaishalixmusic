2b36ee41e1d6b0a28f737a407b8f8d55c7c287c5
Santhosh podili <99010597+Santhupodili@users.noreply.github.com>
2022-03-27 14:42:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,5 +37,5 @@ IMG_4 = getenv("IMG_4", "https://te.legra.ph/file/35700a88d56fd6064822a.png")
 IMG_5 = getenv("IMG_5", "https://telegra.ph/file/63300139d232dc12452ab.png")
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/19814cb8155e0a06a1da9.jpg")
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/9f9a3b5b0822fa621d4c1.jpg")
-YOUTUBE_IMG_URL = getenv("YOUTUBE_IMG_URL", "assets/Youtube.jpeg",
+YOUTUBE_IMG_URL = getenv("YOUTUBE_IMG_URL", "assets/Youtube.jpeg") 
 START_IMG_URL = getenv("START_IMG_URL", None)
