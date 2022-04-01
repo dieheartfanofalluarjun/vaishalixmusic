c09eebea09f4b12e3ef9d30848d5bfc2a4d948d7
Santhosh podili <99010597+Santhupodili@users.noreply.github.com>
2022-04-01 13:06:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ IMG_3 = getenv("IMG_3", "https://te.legra.ph/file/7569d601b34af1bb14570.png")
 IMG_4 = getenv("IMG_4", "https://te.legra.ph/file/35700a88d56fd6064822a.png")
 IMG_5 = getenv("IMG_5", "https://telegra.ph/file/63300139d232dc12452ab.png")
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/19814cb8155e0a06a1da9.jpg")
-UPTIME_IMG = getenv("UPTIME_IMG", "https://te.legra.ph/file/1864dfe3c8a58ca2feac3.jpg")
+UPTIME_IMG = getenv("UPTIME_IMG", "None")
 YOUTUBE_IMG_URL = getenv("YOUTUBE_IMG_URL", "assets/Youtube.jpeg") 
 
 ## BOT IMG
