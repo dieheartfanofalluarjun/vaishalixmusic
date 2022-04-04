d546a0976f2e546832036ec1e1034ff87c325e6e
Santhosh podili <99010597+Santhupodili@users.noreply.github.com>
2022-04-04 13:56:26 +0530

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -11,16 +11,11 @@ from pyrogram.types import (
 def stream_markup(user_id):
   buttons = [
     [
-      InlineKeyboardButton(text="📠ᴍᴇɴᴜ", callback_data=f'stream_menu_panel | {user_id}'),
-      InlineKeyboardButton(text="🚵sᴋɪᴘ", callback_data=f'skip'),
-      InlineKeyboardButton(text="🔊ᴠᴏʟᴜᴍᴇ", callback_data=f'change_volume'),
-    ], 
-    [
       InlineKeyboardButton(text="💘ɴᴇᴛᴡᴏʀᴋ📡", url="https://t.me/santhubotupadates"),
       InlineKeyboardButton(text="💖ɢʀᴏᴜᴘ💝", url="https://t.me/santhuvc"),
     ], 
     [ 
-      InlineKeyboardButton(text="🗑ᴄʟᴏsᴇ", callback_data=f'set_close'),
+      InlineKeyboardButton(text="🗑ʙɪɴ", callback_data=f'set_close'),
     ]
   ]
   return buttons
@@ -29,13 +24,13 @@ def stream_markup(user_id):
 def menu_markup(user_id):
   buttons = [
     [
-      InlineKeyboardButton(text="😶sᴛᴏᴘ", callback_data=f'set_stop | {user_id}'),
-      InlineKeyboardButton(text="🤔ᴘᴀᴜsᴇ", callback_data=f'set_pause | {user_id}'),
-      InlineKeyboardButton(text="🙄ʀᴇsᴜᴍᴇ", callback_data=f'set_resume | {user_id}'),
+      InlineKeyboardButton(text="• sᴛᴏᴘ", callback_data=f'set_stop | {user_id}'),
+      InlineKeyboardButton(text="❣️ ᴘᴀᴜsᴇ", callback_data=f'set_pause | {user_id}'),
+      InlineKeyboardButton(text="(•.•) ʀᴇsᴜᴍᴇ", callback_data=f'set_resume | {user_id}'),
     ],
     [
-      InlineKeyboardButton(text="💔ᴍᴜᴛᴇ", callback_data=f'set_mute | {user_id}'),
-      InlineKeyboardButton(text="💘ᴜɴᴍᴜᴛᴇ", callback_data=f'set_unmute | {user_id}'),
+      InlineKeyboardButton(text="• ᴍᴜᴛᴇ", callback_data=f'set_mute | {user_id}'),
+      InlineKeyboardButton(text="• ᴜɴᴍᴜᴛᴇ", callback_data=f'set_unmute | {user_id}'),
     ],
     [
       InlineKeyboardButton(text="◁", callback_data='stream_home_panel'),
